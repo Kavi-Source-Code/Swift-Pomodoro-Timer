2a9034687e10d0f9956efc26e64e7c633bbde1c1
Kavi <kavivanauken@gmail.com>
2026-02-05 17:05:15 -0800

    Started project

---
+++ Pomodoro Timer/Pomodoro_TimerApp.swift
@@ -0,0 +1,18 @@
+//
+//  Pomodoro_TimerApp.swift
+//  Pomodoro Timer
+//
+//  Created by Kavi Van Auken on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Pomodoro_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
